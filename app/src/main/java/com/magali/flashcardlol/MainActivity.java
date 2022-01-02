258b5671e4b6fc16c83186f5ff8d5f267a5077f2
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-01-02 14:39:07 +0100

    main page

--- app/src/main/java/com/magali/flashcardlol/MainActivity.java
+++ app/src/main/java/com/magali/flashcardlol/MainActivity.java
@@ -2,13 +2,32 @@ package com.magali.flashcardlol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button launchQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        launchQuiz = findViewById(R.id.LauncherButton);
+
+        launchQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToQuiz();
+            }
+        });
+    }
+
+    private void navigateToQuiz() {
+        Intent intent = new Intent(MainActivity.this, QuestionActivity.class);
+        startActivity(intent);
     }
 }
