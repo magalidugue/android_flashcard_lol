d0dee21473a846419481543deff3b1ed77b784f9
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-01-02 14:22:28 +0100

    us2 avec toast

--- app/src/main/java/com/magali/flashcardlol/QuestionActivity.java
+++ app/src/main/java/com/magali/flashcardlol/QuestionActivity.java
@@ -3,12 +3,44 @@ package com.magali.flashcardlol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        RadioGroup radioGroup = findViewById(R.id.RadioGroup);
+
+        submit = findViewById(R.id.Validate);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int answerRadioButtonId = radioGroup.getCheckedRadioButtonId();
+                Log.v("Reponse", "la réponse est" + answerRadioButtonId);
+                if(answerRadioButtonId != -1) {
+                    RadioButton answer = findViewById(answerRadioButtonId);
+                    if(answer.getText().equals("Ahri")) {
+                        Toast.makeText(QuestionActivity.this, "Bonne réponse !", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(QuestionActivity.this, "Désolé la réponse était Ahri ", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(QuestionActivity.this, "Veuillez cocher une réponse ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
